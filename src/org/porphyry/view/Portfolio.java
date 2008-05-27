86ea5a1b2fa5331059bc3b36e3ec900ac4c24c03
aurelien_benel <aurelien_benel@83fa4b38-e21d-0410-9fed-ebbe6ada483a>
2008-05-27 14:26:58 +0000

    Replace SwingWorker 3 with Java 6 SwingWorker backport.

--- src/org/porphyry/view/Portfolio.java
+++ src/org/porphyry/view/Portfolio.java
@@ -30,6 +30,7 @@ import java.net.*;
 import javax.swing.*;
 import javax.swing.border.*;
 import javax.swing.event.*;
+import org.jdesktop.swingworker.SwingWorker; //Java 6 emulation for Java 5
 import org.porphyry.model.LabeledURL;
 import org.porphyry.presenter.ItemSet;
 
