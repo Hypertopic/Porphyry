122e78225fe2da1e7ed77c00b398cbfaa28f858d
Aurore Thomas <aurorethomas4@gmail.com>
2018-04-17 13:58:48 +0200

    TEST and scenarios of FEATURE: Consult topics and attributes related to an item.

---
+++ features/step_definitions/Consult_item_topics_attributes.rb
@@ -0,0 +1,78 @@
+require 'capybara/cucumber'
+require 'selenium/webdriver'
+
+Capybara.run_server = false
+Capybara.default_driver = :selenium_chrome_headless
+Capybara.app_host = "http://localhost:3000"
+Capybara.default_max_wait_time = 10
+
+# Conditions
+
+Soit("le point de vue {string} rattaché au portfolio {string}") do |viewpoint, portfolio|
+  # On the remote servers
+end
+
+Soit("le thème {string} rattaché au point de vue {string}") do |theme, viewpoint|
+  # On the remote servers
+end
+
+Soit("le thème {string} rattaché au thème {string}") do |theme, theme1|
+  # On the remote servers
+end
+
+Soit ("l'item {string} rattaché aux thèmes {string} et {string}") do |item, theme, theme1|
+   # On the remote servers
+end
+
+Soit ("{string} la valeur de l’attribut {string} rattaché à l’item {string}") do |value, attribut ,item|
+   # On the remote servers
+  end
+
+Soit("{string} un item affiché dans la vue courante du visiteur") do |item|
+  case item
+  when "ABT" 
+    true #current configuration
+  else
+    false
+  end
+end
+
+# Events 
+
+Quand("le visiteur sélectionne l’item {string}") do |string|
+  visit "/item/Vitraux%20-%20Recensement/"
+end
+
+# Outcomes
+
+Alors("le titre affiché est {string}") do |item|
+  expect(page).to have_content(item)
+end
+
+Alors("un des attributs affichés a la valeur {string}") do |value|
+  expect(page).to have_content(value)
+end
+
+Alors("un des points de vue affichés est {string}") do |viewpoint|
+  expect(page).to have_content(viewpoint)
+end
+
+Alors("un des thèmes affichés du point de vue {string} est {string}") do |viewpoint, theme|
+  expect(page).to have_content(viewpoint)
+  expect(page).to have_content(theme)
+end
+
+Alors("le titre de l’item affiché est {string}") do |item|
+  expect(page).to have_content(item)
+end
+
+Alors("un des thèmes affichés rattaché au thème {string} est {string}") do |theme, theme1|
+  expect(page).to have_content(theme)
+  expect(page).to have_content(theme1)
+end
+
+Et("{string}la valeur de l’attribut {string} rattaché à l’item {string}") do |value, attribut, item|
+  expect(page).to have_content(value)
+  expect(page).to have_content(attribut)
+  expect(page).to have_content(item)
+end
