3e8dd1ce5bc9a6afa0fb0df414bf3bb809bd05da
heliacerbonne <cerbonne.helia@gmail.com>
2018-05-13 18:04:58 +0200

    FEATURE: Add Bootstrap to the project (see #71)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App/App.jsx';
 
 ReactDOM.render(
