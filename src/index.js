292d08cadfd629be4e894ea7b676708f508c8fe8
heliacerbonne <cerbonne.helia@gmail.com>
2018-06-07 11:21:20 +0200

    FEATURE: add open-iconic to the project (see #72)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'open-iconic/font/css/open-iconic-bootstrap.css';
 import App from './components/App/App.jsx';
 
 ReactDOM.render(
