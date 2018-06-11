672983c1e44890ee0ded9b9b3b0c75214c01528b
Aurelien Benel <aurelien.benel@utt.fr>
2018-06-11 15:39:36 +0200

    TEST: Refactored test of feature #37.
    Improved:
    
    - phrasing,
    - test cases,
    - performance.

--- features/step_definitions/portfolio.rb
+++ features/step_definitions/portfolio.rb
@@ -16,6 +16,14 @@ Soit("le corpus {string} rattaché au portfolio {string}") do |viewpoint, portfo
   # On the remote servers
 end
 
+Soit("la rubrique {string} contenue dans la rubrique {string}") do |topic1, topic2|
+  # On the remote servers
+end
+
+Soit("{int} items décrits par {string} et {string}") do |itemsNb, topic1, topic2|
+  # On the remote servers
+end
+
 Soit("{string} le portfolio spécifié dans la configuration") do |portfolio|
   case portfolio
   when "vitraux" 
@@ -27,6 +35,14 @@ Soit("{string} le portfolio spécifié dans la configuration") do |portfolio|
   end
 end
 
+Soit("{string} le portfolio ouvert") do |portfolio|
+  visit "/"
+end
+
+Soit("{string} une des rubriques développées") do |topic|
+  find_link(topic).sibling('.Bullet').click
+end
+
 # Events 
 
 Quand("un visiteur ouvre la page d'accueil du site") do
@@ -35,6 +51,10 @@ end
 
 Quand("un visiteur ouvre la page d‘accueil d‘un site dont l‘adresse commence par {string}") do |portfolio|
   visit "/"
+end
+
+Quand("on sélectionne la rubrique {string}") do |topic|
+  click_on topic
 end
 
 # Outcomes
@@ -51,27 +71,7 @@ Alors("un des corpus affichés est {string}") do |corpus|
   expect(page).to have_content corpus
 end
 
-
-
-########
-#  For every topic, get the number of selected items assigned to it (see #37)
-#  get_number_selected_items_assigned_to_topic.feature
-########
-Soit("la page d'accueil chargée") do
-  visit "/"
+Alors("il doit y avoir au moins {int} items sélectionnés décrits par {string}") do |itemsNb, topic|
+  expect(find_link(topic).sibling('span').text.scan(/\d+/)[0].to_i).to be >= itemsNb 
 end
 
-Soit("le topic {string} présent sur la page") do |topic|
-  expect(page).to have_content(topic)
-end
-
-Quand("un utilisateur développe le topic {string} et clique sur le topic {string}") do |parentTopic, childTopic|
-  page.all(:css, '.Bullet').each do |el|
-    el.click
-  end
-  click_on(childTopic) 
-end
-
-Alors("Il doit y avoir au moins {int} items inscrits à côté de {string}") do |nbItems, topic|
-  expect(page).to have_content(topic+" ("+nbItems.to_s+")")
-end
