d711b086d084a4945d7c7f5baee8a0e902f690d3
Quoc Nhat Han TRAN <quoc_nhat_han.tran@utt.fr>
2018-06-15 10:55:46 +0200

    TEST: Create a sibling topic (close #67)
    Co-Authored-By: Frederic HONG <frederic.hong@utt.fr>
    Co-Authored-By: Caroline PRUVOST <caroline.dequidt@worldonline.fr>

---
+++ features/create_sibling.feature
@@ -0,0 +1,8 @@
+#language: fr
+
+Fonctionnalité: Create a sibling topic
+
+Scénario: Créer une soeur à une catégorie
+Soit l`utilisateur dans la page de modification du point de vue "Histoire de l'art"
+Quand l`utilisateur crée une catégorie soeur "Linard Gontier sibling" de "Linard Gontier" qui est la fille de la catégorie "Atelier Gontier"
+Alors la catégorie "Linard Gontier sibling" apparait dans l`arborescence à côté de "Linard Gontier"
---
+++ features/step_definitions/create_sibling.rb
@@ -0,0 +1,26 @@
+﻿require 'capybara/cucumber'
+require 'selenium/webdriver'
+
+Capybara.run_server = false
+Capybara.default_driver = :selenium_chrome_headless
+Capybara.app_host = "http://localhost:3000"
+Capybara.default_max_wait_time = 10
+
+# Outcomes
+
+Soit("l`utilisateur dans la page de modification du point de vue {string}") do |pdv|
+  visit "/"
+  find("h3", :text => pdv).click_link("✏️")
+end
+
+Quand("l`utilisateur crée une catégorie soeur {string} de {string} qui est la fille de la catégorie {string}") do |sibling, topic, parent|
+  page.find('span', text: parent).hover
+  click_on('➕')
+  find('input').set(sibling)
+  find('input').native.send_keys(:return)  
+end
+
+Alors("la catégorie {string} apparait dans l`arborescence à côté de {string}") do |sibling, topic|
+  expect(page).to have_content(sibling)
+  expect(page).to have_content(topic)
+end
