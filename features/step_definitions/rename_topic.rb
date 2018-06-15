b47f29afe5f1e5b088511a4b972f8d5556cdd58f
Quoc Nhat Han TRAN <quoc_nhat_han.tran@utt.fr>
2018-06-15 11:25:22 +0200

    TEST: Rename topic (close #44)
    Co-Authored-By: Frederic HONG <frederic.hong@utt.fr>
    Co-Authored-By: Caroline PRUVOST <caroline.dequidt@worldonline.fr>

---
+++ features/step_definitions/rename_topic.rb
@@ -0,0 +1,26 @@
+﻿require 'capybara/cucumber'
+require 'selenium/webdriver'
+
+Capybara.run_server = false
+Capybara.default_driver = :selenium_chrome_headless
+Capybara.app_host = "http://localhost:3000"
+Capybara.default_max_wait_time = 10
+
+Soit("la catégorie {string} sous {string}") do |topicToBeRenamed, parent|
+  page.find('span', text: parent).hover
+  click_on('➕')
+  find('input').set(topicToBeRenamed)
+  find('input').native.send_keys(:return)  
+end
+
+Quand("l`utilisateur change {string} en {string}") do |topicToBeRenamed, topicRenamed|
+  page.find('span', text: topicToBeRenamed).hover
+  click_on("✏️")
+  find('input').set(topicRenamed)
+  find('input').native.send_keys(:return)  
+end
+
+Alors("la catégorie {string} remplace {string}") do |topicRenamed, topicToBeRenamed|
+  expect(page).to have_content(topicRenamed)
+  expect(page).not_to have_content(topicToBeRenamed)
+end
