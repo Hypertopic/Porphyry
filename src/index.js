c5e0f8722e9ae2b877be9ca39a53b63836c36479
JoJoDU <qiaoqiandu@gmail.com>
2019-06-04 09:23:32 +0200

    merge

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import 'open-iconic/font/css/open-iconic-bootstrap.css';
 import App from './components/App/App.jsx';
 import Languages from 'languages-js';
 
-Languages.init(['en_EN', 'fr_FR'], './languages', () => {
+Languages.init(['en_EN', 'fr_FR'], './languages/', () => {
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
-})
+});
