657f79fc4e1bc692b7d4386c6aab68eea8b9d6db
JoJoDU <qiaoqiandu@gmail.com>
2019-06-07 12:42:39 +0200

    initialisation de la branche

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import './styles/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'open-iconic/font/css/open-iconic-bootstrap.css';
 import App from './components/App/App.jsx';
+import Languages from 'languages-js';
 
+Languages.init(['en_EN', 'fr_FR'], './languages/', () => {
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+})
