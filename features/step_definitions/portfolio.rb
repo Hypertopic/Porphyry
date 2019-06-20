31c57426a929d3c1fc2c833a06444877811da1ef
lvauchez <louis.vauchez@hotmail.fr>
2019-06-20 15:56:01 +0200

    TEST: Create an item (#192)
    Co-authored-by: maxiloud <maxime.trinquard@utt.fr>
    Co-authored-by: Jadestanzie <jade.venault@utt.fr>
    Co-authored-by: nhuchon <nhuchon@hotmail.fr>

--- features/step_definitions/portfolio.rb
+++ features/step_definitions/portfolio.rb
@@ -7,6 +7,7 @@ Capybara.javascript_driver = :cuprite
 Capybara.app_host = "http://localhost:3000"
 Capybara.default_max_wait_time = 10
 
+
 def getUUID(itemName)
   uuid = nil
   case itemName
@@ -22,7 +23,20 @@ def getUUID(itemName)
   return uuid
 end
 
+
+def getPassword(username)
+  password = nil
+  case username
+  when "alice"
+    password = "whiterabbit"
+  end
+  return password
+end
 # Conditions
+
+Soit("l'item {string} existant") do |item|
+  # On the remote servers
+end
 
 Soit("le point de vue {string} rattaché au portfolio {string}") do |viewpoint, portfolio|
   # On the remote servers
@@ -92,6 +106,14 @@ Soit("la liste des rubriques sélectionnées est vide") do
   visit "/"
 end
 
+Soit ("l'utilisateur {string} connecté") do |username|
+  click_link('Se connecter...')
+  find('input[placeholder="nom d\'utilisateur"]').set username
+  find("input[placeholder='mot de passe']").set getPassword(username)
+  click_on('Se connecter')
+  expect(page).to have_content(username)
+end
+
 # Events
 
 Quand("un visiteur ouvre la page d'accueil du site") do
@@ -108,6 +130,13 @@ end
 
 Quand("on choisit l'item {string}") do |item|
   click_on item
+end
+
+Quand ("l'utilisateur crée un item {string} dans le corpus {string}") do |name, corpus|
+  find_button(:id => corpus).click
+  expect(page).to have_content("undefined")
+  find_by_id("new-attribute").set "name:#{name}"
+  find_button(class: ['btn', 'btn-sm', 'ValidateButton']).click
 end
 
 # Outcomes
@@ -139,4 +168,3 @@ end
 Alors ("l'item {string} n'est pas affiché") do |item|
   expect(page).not_to have_content item
 end
-
