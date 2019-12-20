b7dff6c33ab3ded7539b8030cdce85a42ae73207
Aurelien Benel <aurelien.benel@utt.fr>
2019-12-20 21:15:36 +0100

    PROCESS: Acceptance testing is now dockerized.

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,5 +4,10 @@ require 'capybara/cuprite'
 Capybara.run_server = false
 Capybara.default_driver = :cuprite
 Capybara.javascript_driver = :cuprite
-Capybara.app_host = "http://localhost:3000"
-Capybara.default_max_wait_time = 10
+Capybara.app_host = ENV["APP_HOST"] || "http://localhost:3000"
+Capybara.register_driver(:cuprite) do |app|
+  Capybara::Cuprite::Driver.new(app,
+    browser_options: { 'no-sandbox': nil },
+    timeout: 30
+  )
+end
