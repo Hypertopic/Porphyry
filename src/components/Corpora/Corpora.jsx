Hypertopic/Porphyry

commit 3f13a3c27a0bd1de2c4d4c12810987060c8a51e6

FEATURE: Create an item (closes #192)
Co-authored-by: maxiloud <maxime.trinquard@utt.fr>
Co-authored-by: Jadestanzie <jade.venault@utt.fr>
Co-authored-by: nhuchon <nhuchon@hotmail.fr>

src/components/Corpora/Corpora.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import getConfig from '../../config/config.js';
+import ItemCreator from '../Item/ItemCreator.jsx';
 
 // Get the configured list display mode
 let listView = getConfig('listView', {
@@ -15,11 +16,14 @@ class Corpora extends Component {
     let items = this._getItems();
     let count = this.props.items.length;
     let total = this.props.from;
+    let listIds = this.props.ids.map((item) =>
+      <div>{item} <ItemCreator corpus={item} /></div>
+    );
     return(
       <div className="col-md-8 p-4">
         <div className="Subject">
           <h2 className="h4 font-weight-bold text-center">
-            {this.props.ids.join(' + ')}
+            {listIds}
             <span className="badge badge-pill badge-light ml-4">{count} / {total}</span>
           </h2>
           <div className="Items m-3">

src/components/Item/ItemCreator.jsx
@@ -1,0 +1,39 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+
+import Hypertopic from 'hypertopic';
+import conf from '../../config/config.json';
+
+const db = new Hypertopic(conf.services);
+
+const _log = (x) => console.log(JSON.stringify(x, null, 2));
+const _error = (x) => console.error(x.message);
+
+class ItemCreator extends Component {
+  constructor(props) {
+    super();
+  }
+
+  render() {
+    return (
+      <button className="btn btn-light creationItemButton" id={this.props.corpus} onClick={_ => this._newItem()}>
+        +
+      </button>
+    );
+  }
+
+  _newItem() {
+    db.post({item_corpus: this.props.corpus})
+    .then((data) => {
+      _log(data);
+      this._goToNewItem(data);
+    })
+    .catch(_error);
+  }
+
+  _goToNewItem(data) {
+    this.props.history.push(`/item/${data.item_corpus}/${data._id}`);
+  }
+}
+
+export default withRouter(ItemCreator);

src/components/Portfolio/Portfolio.jsx
@@ -189,7 +189,7 @@ class Portfolio extends Component {
           for (let itemId in data[corpus.id]) {
             if (!['id','name','user'].includes(itemId)) {
               let item = data[corpus.id][itemId];
-              if (!item.name || !item.name.length || !item.thumbnail || !item.thumbnail.length) {
+              if (!item.name || !item.name.length) {
                 console.log(itemId, "has no name or thumbnail!", item);
               } else {
                 item.id = itemId;

src/styles/App.css
@@ -288,6 +288,11 @@ div.autosuggest {
   margin-bottom: 1rem;
 }
 
+.creationItemButton{
+  padding: 1px;
+  border-radius: 15px;
+}
+
 .Return {
   position: fixed;
   width:60px;

commit 8a037ecc16bd167ce2ee2f248b9f96200ad3ac8a

FIX: React warning caused by 3f13a3c2.

src/components/Corpora/Corpora.jsx
@@ -14,8 +14,8 @@ class Corpora extends Component {
     let items = this._getItems();
     let count = this.props.items.length;
     let total = this.props.from;
-    let listIds = this.props.ids.map((item) =>
-      <div>{item} <ItemCreator corpus={item} conf={this.props.conf} /></div>
+    let listIds = this.props.ids.map((corpus) =>
+      <div key={corpus}>{corpus} <ItemCreator corpus={corpus} conf={this.props.conf} /></div>
     );
     return (
       <div className="col-md-8 p-4">