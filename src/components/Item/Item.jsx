Hypertopic/Porphyry

commit c489e05729ab1fb44c167c5ca41c83643722bb26

PROCESS: Settings are no more included in build.
Dockerization (e0953db9a2333e8bbd749c9d) would be useless without it.

src/components/Item/Item.jsx
@@ -3,24 +3,20 @@ import { Link } from 'react-router-dom';
 import Hypertopic from 'hypertopic';
 import groupBy from 'json-groupby';
 import Autosuggest from 'react-autosuggest';
-import conf from '../../config/config.json';
-import getConfig from '../../config/config.js';
+import conf from '../../config/config.js';
 import Header from '../Header/Header.jsx';
 import Authenticated from '../Authenticated/Authenticated.jsx';
 import TopicTree from '../Outliner/TopicTree.js';
 
 import '../../styles/App.css';
 
-let hypertopic = new Hypertopic(conf.services);
-
-// Get the configured item display mode
-let itemView = getConfig('itemView', {
+let itemView = {
   mode: 'picture',
   name: 'name',
   image: 'resource',
   linkTo: 'resource',
   hiddenProps: ['topic', 'resource', 'thumbnail']
-});
+};
 
 function getString(obj) {
   if (Array.isArray(obj)) {
@@ -43,7 +39,6 @@ class Item extends Component {
     this._getOrCreateItem = this._getOrCreateItem.bind(this);
     this._submitAttribute = this._submitAttribute.bind(this);
     this._deleteAttribute = this._deleteAttribute.bind(this);
-    this.user=conf.user || window.location.hostname.split('.', 1)[0];
   }
 
   render() {
@@ -52,9 +47,9 @@ class Item extends Component {
     let viewpoints = this._getViewpoints();
     return (
       <div className="App container-fluid">
-        <Header />
+        <Header conf={conf} />
         <div className="Status row h5">
-          <Authenticated/>
+          <Authenticated conf={conf} />
           <Link to="/" className="badge badge-pill badge-light TopicTag">
             <span className="badge badge-pill badge-dark oi oi-chevron-left"> </span> Retour à l'accueil
           </Link>
@@ -124,7 +119,8 @@ class Item extends Component {
     clearInterval(this._timer);
   }
 
-  _getOrCreateItem() {
+  async _getOrCreateItem() {
+    let hypertopic = new Hypertopic((await conf).services);
     return hypertopic.get({_id: this.props.match.params.item})
     .catch(e => {
       return {
@@ -134,9 +130,11 @@ class Item extends Component {
     });
   }
 
-  _fetchItem() {
+  async _fetchItem() {
     let uri = this.props.match.url;
     let params = this.props.match.params;
+    let SETTINGS = await conf;
+    let hypertopic = new Hypertopic(SETTINGS.services);
     return hypertopic.getView(uri).then((data) => {
       let item = data[params.corpus][params.item];
       let itemTopics = (item.topic) ? groupBy(item.topic, ['viewpoint']) : {};
@@ -146,7 +144,7 @@ class Item extends Component {
       }
       item.topic=topics;
       this.setState({item});
-    }).then(() => hypertopic.getView(`/user/${this.user}`))
+    }).then(() => hypertopic.getView(`/user/${SETTINGS.user}`))
       .then((data) => {
       let user = data[this.user] || {};
       if (user.viewpoint) {
@@ -228,8 +226,9 @@ class Item extends Component {
     );
   }
 
-  _setAttribute(key, value) {
+  async _setAttribute(key, value) {
     if (key!=='' && value!=='') {
+      let hypertopic = new Hypertopic((await conf).services);
       let attribute = {[key]: [value]};
       return this._getOrCreateItem()
         .then(x => Object.assign(x, attribute))
@@ -260,7 +259,8 @@ class Item extends Component {
     return false;
   }
 
-  _deleteAttribute(key) {
+  async _deleteAttribute(key) {
+    let hypertopic = new Hypertopic((await conf).services);
     const _error = (x) => console.error(x.message);
     this._getOrCreateItem()
       .then(x => {
@@ -277,7 +277,8 @@ class Item extends Component {
       .catch(_error);
   }
 
-  _assignTopic(topicToAssign, viewpointId) {
+  async _assignTopic(topicToAssign, viewpointId) {
+    let hypertopic = new Hypertopic((await conf).services);
     return this._getOrCreateItem()
       .then(data => {
         data.topics=data.topics || {};
@@ -298,7 +299,8 @@ class Item extends Component {
   }
 
 
-  _removeTopic(topicToDelete) {
+  async _removeTopic(topicToDelete) {
+    let hypertopic = new Hypertopic((await conf).services);
     if (window.confirm('Voulez-vous réellement que l\'item affiché ne soit plus décrit à l\'aide de cette rubrique ?')) {
       return this._getOrCreateItem()
         .then(data => {
@@ -688,9 +690,9 @@ class Viewpoint extends Component {
     this._fetchViewpoint();
   }
 
-  _fetchViewpoint() {
-    let uri = '/viewpoint/' + this.props.id;
-    hypertopic.getView(uri).then((data) => {
+  async _fetchViewpoint() {
+    let hypertopic = new Hypertopic((await conf).services);
+    hypertopic.getView(`/viewpoint/${this.props.id}`).then((data) => {
       let viewpoint = data[this.props.id];
       let name = viewpoint.name;
       let topics = viewpoint;
@@ -701,8 +703,9 @@ class Viewpoint extends Component {
     });
   }
 
-  createTopic(name,parent) {
+  async createTopic(name,parent) {
     var newId;
+    let hypertopic = new Hypertopic((await conf).services);
     return hypertopic.get({ _id: this.props.id })
       .then(x => {
         var topicTree=new TopicTree(x.topics);

public/config.yml
@@ -1,0 +1,4 @@
+user: vitraux
+services:
+  - http://argos2.test.hypertopic.org
+  - http://steatite.hypertopic.org

src/components/Authenticated/Authenticated.jsx
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
-import conf from '../../config/config.json';
-
-const SESSION_URI = conf.services[0] + '/_session';
 
 class Authenticated extends Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       user: '',
       ask: false
@@ -40,6 +37,14 @@ class Authenticated extends Component {
     );
   }
 
+  requestSession(options) {
+    return this.props.conf.then(x => {
+      options = options || {};
+      options.credentials = 'include';
+      return fetch(x.services[0] + '/_session',  options)
+    });
+  }
+
   handleAsk(e) {
     e.preventDefault();
     this.setState({ask: true});
@@ -57,7 +62,7 @@ class Authenticated extends Component {
   }
 
   _fetchSession() {
-    fetch(SESSION_URI, {credentials: 'include'})
+    this.requestSession()
       .then(x => x.json())
       .then(x => this.setState({user: x.name || x.userCtx.name}))
       .catch(() => this.setState({user: ''}));
@@ -65,13 +70,12 @@ class Authenticated extends Component {
 
   _openSession() {
     let user = this.login.value;
-    fetch(SESSION_URI, {
+    this.requestSession({
       method:'POST',
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
       },
-      body:`name=${user}&password=${this.password.value}`,
-      credentials:'include'
+      body:`name=${user}&password=${this.password.value}`
     })
       .then(x => {
         if (!x.ok) throw new Error('Bad credentials!');
@@ -81,7 +85,7 @@ class Authenticated extends Component {
   }
 
   _closeSession() {
-    fetch(SESSION_URI, {method:'DELETE', credentials:'include'})
+    this.requestSession({method:'DELETE'})
       .then(() => this.setState({user: ''}));
   }
 

src/components/Corpora/Corpora.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import getConfig from '../../config/config.js';
 import ItemCreator from '../Item/ItemCreator.jsx';
 
-// Get the configured list display mode
-let listView = getConfig('listView', {
+let listView = {
   mode: 'picture',
   name: 'name',
   image: 'thumbnail'
-});
+};
 
 class Corpora extends Component {
 
@@ -17,7 +15,7 @@ class Corpora extends Component {
     let count = this.props.items.length;
     let total = this.props.from;
     let listIds = this.props.ids.map((item) =>
-      <div>{item} <ItemCreator corpus={item} /></div>
+      <div>{item} <ItemCreator corpus={item} conf={this.props.conf} /></div>
     );
     return(
       <div className="col-md-8 p-4">

src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import conf from '../../config/config.json';
 
 class Header extends Component {
-  constructor() {
-    super();
-    this.user = conf.user || window.location.hostname.split('.', 1)[0];
+
+  constructor(props) {
+    super(props);
+    this.state = {user: ''};
+    props.conf.then(x => {this.setState({user: x.user})});
   }
 
   render() {
@@ -15,10 +16,11 @@ class Header extends Component {
         <div className="col-lg-2 col-md-3 col-sm-4">
           <input className="form-control" type="text" placeholder="Rechercher..."/>
         </div>
-        <h1 className="text-center col-lg-8 col-md-6 col-sm-8"><Link to="/">{this.user}</Link></h1>
+        <h1 className="text-center col-lg-8 col-md-6 col-sm-8"><Link to="/">{this.state.user}</Link></h1>
       </header>
     );
   }
+
 }
 
 export default Header;

src/components/Item/ItemCreator.jsx
@@ -1,18 +1,11 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
-
 import Hypertopic from 'hypertopic';
-import conf from '../../config/config.json';
-
-const db = new Hypertopic(conf.services);
 
 const _log = (x) => console.log(JSON.stringify(x, null, 2));
 const _error = (x) => console.error(x.message);
 
 class ItemCreator extends Component {
-  constructor(props) {
-    super();
-  }
 
   render() {
     return (
@@ -22,7 +15,8 @@ class ItemCreator extends Component {
     );
   }
 
-  _newItem() {
+  async _newItem() {
+    let db = new Hypertopic((await this.props.conf).services);
     db.post({item_corpus: this.props.corpus})
     .then((data) => {
       _log(data);

src/components/Outliner/Outliner.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Hypertopic from 'hypertopic';
-import conf from '../../config/config.json';
+import conf from '../../config/config.js';
 import Header from '../Header/Header.jsx';
 import Authenticated from '../Authenticated/Authenticated.jsx';
 import TopicTree from './TopicTree.js';
 
 import '../../styles/App.css';
-
-const db = new Hypertopic(conf.services);
 
 const _log = (x) => console.log(JSON.stringify(x, null, 2));
 const _error = (x) => console.error(x.message);
@@ -20,7 +18,6 @@ class Outliner extends React.Component {
     this.state = { };
     this.changing=false;
     this.topicTree=new TopicTree({});
-    this.user = conf.user || window.location.hostname.split('.', 1)[0];
   }
 
   render() {
@@ -28,9 +25,9 @@ class Outliner extends React.Component {
     let topic={name:this.state.title};
     return (
       <div className="App container-fluid">
-        <Header />
+        <Header conf={conf} />
         <div className="Status row h5">
-          <Authenticated/>
+          <Authenticated conf={conf} />
           <Link to="/" className="badge badge-pill badge-light TopicTag">
             <span className="badge badge-pill badge-dark oi oi-chevron-left"> </span> Retour à l'accueil
           </Link>
@@ -74,13 +71,13 @@ class Outliner extends React.Component {
     }
   }
 
-  _newVP(e) {
+  async _newVP(e) {
     e.preventDefault();
     let title = e.target.newTitle.value;
-    if (!title) {
-      return;
-    }
-    db.post({ _id: this.props.match.params.id, viewpoint_name: title, topics: {}, users: [this.user] })
+    if (!title) return;
+    let SETTINGS = await conf;
+    new Hypertopic(SETTINGS.services)
+      .post({_id: this.props.match.params.id, viewpoint_name: title, topics: {}, users: [SETTINGS.user]})
       .then(_log)
       .then(_ => this.setState({ title }))
       .then(_ => this._fetchData())
@@ -224,8 +221,9 @@ class Outliner extends React.Component {
     clearInterval(this._timer);
   }
 
-  applyChange() {
+  async applyChange() {
     if (!this.changing) {
+      let db = new Hypertopic((await conf).services);
       this.changing=db.get({ _id: this.props.match.params.id })
         .then(data => {
           data.topics ={};
@@ -250,13 +248,14 @@ class Outliner extends React.Component {
     return this.changing;
   }
 
-  _fetchData() {
+  async _fetchData() {
     if (!this.changing) {
-    return db.get({ _id: this.props.match.params.id })
-      .then(x => {
-        this.setState({ topics: x.topics, title: x.viewpoint_name });
-        this.topicTree=new TopicTree(x.topics);
-      });
+      new Hypertopic((await conf).services)
+        .get({ _id: this.props.match.params.id })
+        .then(x => {
+          this.setState({ topics: x.topics, title: x.viewpoint_name });
+          this.topicTree = new TopicTree(x.topics);
+        });
     } else {
       return true;
     }
@@ -269,7 +268,6 @@ class Node extends React.Component {
   constructor() {
     super();
     this.state = { edit: false, active: false, open: true };
-    this.user = conf.user || window.location.hostname.split('.', 1)[0];
   }
 
   render = () => {

src/components/Portfolio/Portfolio.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import by from 'sort-by';
 import queryString from 'query-string';
 import Hypertopic from 'hypertopic';
-import conf from '../../config/config.json';
+import conf from '../../config/config.js';
 import Viewpoint from '../Viewpoint/Viewpoint.jsx';
 import Corpora from '../Corpora/Corpora.jsx';
 import Header from '../Header/Header.jsx';
@@ -22,7 +22,6 @@ class Portfolio extends Component {
       selectedItems: [],
       topicsItems: new Map()
     };
-    this.user = conf.user || window.location.hostname.split('.', 1)[0];
     this._updateSelection();
   }
 
@@ -32,9 +31,9 @@ class Portfolio extends Component {
 
     return (
       <div className="App container-fluid">
-        <Header />
+        <Header conf={conf} />
         <div className="Status row h5 text-center">
-          <Authenticated/>
+          <Authenticated conf={conf} />
           <Status selectionJSON={this.selectionJSON} viewpoints={this.state.viewpoints}/>
         </div>
         <div className="container-fluid">
@@ -143,11 +142,12 @@ class Portfolio extends Component {
     this.setState({selectedItems, topicsItems});
   }
 
-  _fetchAll() {
-    const hypertopic = new Hypertopic(conf.services);
-    return hypertopic.getView(`/user/${this.user}`)
+  async _fetchAll() {
+    let SETTINGS = await conf;
+    let hypertopic = new Hypertopic(SETTINGS.services);
+    return hypertopic.getView(`/user/${SETTINGS.user}`)
       .then(data => {
-        let user = data[this.user] || {};
+        let user = data[SETTINGS.user] || {};
         user = {
           viewpoint: user.viewpoint || [],
           corpus: user.corpus || []
@@ -208,7 +208,7 @@ class Portfolio extends Component {
   _getCorpora() {
     let ids = this.state.corpora.map(c => c.id);
     return (
-      <Corpora ids={ids} from={this.state.items.length} items={this.state.selectedItems} />
+      <Corpora ids={ids} from={this.state.items.length} items={this.state.selectedItems} conf={conf} />
     );
   }
 }

commit 53e686fb03f8855eeb4749faa2d6f4d1155ee345

FIX: Should display unused viewpoints in item page (#135).
Regression caused by c489e057.

src/components/Item/Item.jsx
@@ -129,7 +129,7 @@ class Item extends Component {
       this.setState({item});
     }).then(() => hypertopic.getView(`/user/${SETTINGS.user}`))
       .then((data) => {
-      let user = data[this.user] || {};
+      let user = data[SETTINGS.user] || {};
       if (user.viewpoint) {
         let topic=this.state.item.topic;
         for (let vp of user.viewpoint) {