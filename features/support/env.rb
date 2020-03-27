Hypertopic/Porphyry

commit 0ece0c80bb429eff7bdc3db638d7c56b8e4d5c8e

TEST: Set an item's attribute (see #31).

features/support/env.rb
@@ -23,7 +23,6 @@ def getURI(name)
 end
 
 def getUUID(name)
-  uuid = nil
   case name
     when "Abram/Abraham"
       uuid = "f1520229979b11428f94a004f880c022"
@@ -34,6 +33,8 @@ def getUUID(name)
     when "vers 1520"
       uuid = "e01a7cb572461a43a22aa8f771235cb6"
   end
+  range = [*'0'..'9',*'A'..'F']
+  uuid = Array.new(36){ range.sample }.join
   return uuid
 end
 

features/attribute_set.feature
@@ -1,0 +1,9 @@
+#language: fr
+
+Fonctionnalité: Décrire un item à l’aide d’attributs
+
+Scénario:
+  Soit un item en cours de création
+  Et l'utilisateur "alice" connecté
+  Quand l'utilisateur indique "Musée de Vauluisant, Troyes" comme valeur de l'attribut "spatial"
+  Alors la valeur de l'attribut "spatial" est "Musée de Vauluisant, Troyes"

features/step_definitions/context.rb
@@ -48,3 +48,7 @@ end
 Soit("{string} l'item affiché") do |item|
   visit getURI(item)
 end
+
+Soit("un item en cours de création") do
+  visit '/item/Vitraux - Bénel/' + getUUID('')
+end

features/step_definitions/event.rb
@@ -27,3 +27,7 @@ Quand("on choisit la rubrique {string}") do |topic|
   click_on topic
 end
 
+Quand("l'utilisateur indique {string} comme valeur de l'attribut {string}") do |value, attribute|
+  find_by_id('new-attribute').set "#{attribute}:#{value}"
+  find_button(class: ['btn', 'btn-sm', 'ValidateButton']).click
+end

commit 4b5970cf629b6d39c5d876abc7f7204789cf724e

PROCESS: Fixed tests (broken by commit 0ece0c80).

features/support/env.rb
@@ -32,9 +32,10 @@ def getUUID(name)
       uuid = "fed64e22e60941409ad45c167fc396b8"
     when "vers 1520"
       uuid = "e01a7cb572461a43a22aa8f771235cb6"
+    else
+      range = [*'0'..'9',*'A'..'F']
+      uuid = Array.new(36){ range.sample }.join
   end
-  range = [*'0'..'9',*'A'..'F']
-  uuid = Array.new(36){ range.sample }.join
   return uuid
 end
 