1e93b4f5ffb3eab8481fdf070efe6feb8090f02d
wikolas21 <nyk0mo@gmail.com>
2020-04-30 15:06:47 +0200

    FEATURE : Ajout feature pour les liens (voir #208) ~

--- src/components/itemPage/Attribute.jsx
+++ src/components/itemPage/Attribute.jsx
@@ -89,6 +89,13 @@ function AttributeValue(props) {
       />
     </div>
   );
+
+  if(props.value.startsWith("http")) return (
+    <a href={props.value} className="Value">
+      {props.value}
+    </a>
+  );
+
   return (
     <div className="Value">
       {props.value}
