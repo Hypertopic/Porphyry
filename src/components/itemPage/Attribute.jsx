f12eb541af3f7b028cdc67644a329fa426a92422
Valentin LOUERAT <valentin.louerat@gmail.com>
2020-04-30 17:03:23 +0200

    FEATURE: Open a ressource (closes #208)

--- src/components/itemPage/Attribute.jsx
+++ src/components/itemPage/Attribute.jsx
@@ -89,6 +89,16 @@ function AttributeValue(props) {
       />
     </div>
   );
+
+  if(props.value && props.value.startWith('http'))
+  return (
+    <div className="Value">
+      <a href={props.value}>
+        {props.value}
+      </a>
+    </div>
+  );
+
   return (
     <div className="Value">
       {props.value}
