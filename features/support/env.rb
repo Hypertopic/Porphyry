a3f57029d64d057ada4035ad126ea4c6714faae9
Antoine Guyot <antoineguyot49@gmail.com>
2021-06-16 18:30:22 +0200

    SCENARIO: Check the optimization of an image (see #383)

--- features/support/env.rb
+++ features/support/env.rb
@@ -23,6 +23,8 @@ def getURI(name)
       uri = "/item/Vitraux%20-%20Bénel/8a1750b17b11944108efaac593f4448e4e9f966b"
     when "AXN 009"
       uri = "/item/Vitraux%20-%20Bénel/dff21da25a2bebd654e81536fc5aaa8a63ab6e4f"
+    when "SJ 001"
+      uri = "/item/Vitraux%20-%20Bénel/8d27115e20f9439b627ac511ade9a05bbe509130"
   end
   return uri
 end
