549fbb9d6c8fffbb0d79710d8b5555c5b3779aff
Antoine Guyot <antoineguyot49@gmail.com>
2021-06-16 18:49:23 +0200

    TEST: The application should be quickly usable on consulting an item (see #383)
    Co-Authored-By: Albert Elombo <albertelombo@gmail.com>

--- features/support/env.rb
+++ features/support/env.rb
@@ -23,8 +23,10 @@ def getURI(name)
       uri = "/item/Vitraux%20-%20Bénel/8a1750b17b11944108efaac593f4448e4e9f966b"
     when "AXN 009"
       uri = "/item/Vitraux%20-%20Bénel/dff21da25a2bebd654e81536fc5aaa8a63ab6e4f"
-	when "SR 005"
-	  uri = "/item/Vitraux%20-%20Bénel/493f2a3702be0e9a67a6fd29700fd4dff8f6986a"
+    when "SR 005"
+      uri = "/item/Vitraux%20-%20Bénel/493f2a3702be0e9a67a6fd29700fd4dff8f6986a"
+    when "SJ 001"
+      uri = "/item/Vitraux%20-%20Bénel/8d27115e20f9439b627ac511ade9a05bbe509130"
   end
   return uri
 end
