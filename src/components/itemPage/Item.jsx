Hypertopic/Porphyry

commit f68d4fedf3fa7b0ccafcb0f1a8f77db7fbb2962e

FEATURE: Browse items while moving in a physical space (see #369).
To enable the feature add a portfolio in the YAML config file and add to it a boolean true called visitMap

Made with the psychological support of people listed below as co-authors.

Co-authored-by: aurelien45100 <aurlien45@hotmail.fr>
Co-authored-by: lusardia <alice.lusardi@utt.fr>
Co-authored-by: dylan-correia <79135837+dylan-correia@users.noreply.github.com>

src/components/itemPage/Item.jsx
@@ -27,8 +27,15 @@ class Item extends Component {
     super();
     this.state = {
       attributeInputValue: '',
-      item: {topic: []}
+      item: { topic: [] },
+      visitMap: false
     };
+    conf.then(settings => {
+      if (settings.portfolio && settings.portfolio[settings.user])
+        this.setState({
+          visitMap: settings.portfolio[settings.user].visitMap || false,
+        });
+    });
   }
 
   render() {
@@ -36,6 +43,23 @@ class Item extends Component {
     let attributes = this._getAttributes();
     let viewpoints = this._getViewpoints();
     let sameNameBlock = this._getSameNameBlock();
+    const { visitMap } = this.state;
+    const urlParams = new URLSearchParams(window.location.search);
+    const visitName = urlParams.get('visit');
+    const goBackUrlParams = new URLSearchParams();
+    if (visitMap && visitName) {
+      goBackUrlParams.set('t', JSON.stringify({
+        'type': 'intersection',
+        'data': [{
+          'type': 'intersection',
+          'selection': [
+            `spatial : ${visitName}`
+          ],
+          'exclusion': []
+        }]
+      }));
+      goBackUrlParams.set('mode', 'visit');
+    }
     return (
       <div className="App container-fluid">
         <Header conf={conf} />
@@ -43,9 +67,12 @@ class Item extends Component {
           <title>{name}</title>
         </Helmet>
         <div className="Status row h5">
-          <Link to="/" className="badge badge-pill badge-light TopicTag">
+          <Link to={{
+            pathname: '/',
+            search: decodeURIComponent(goBackUrlParams.toString())
+          }} className="badge badge-pill badge-light TopicTag">
             <span className="badge badge-pill badge-dark oi oi-chevron-left"> </span>
-            <Trans>Retour à l'accueil</Trans>
+            <Trans>{visitName ? 'Retour à la visite' : 'Retour à l\'accueil'}</Trans>
           </Link>
         </div>
         <div className="container-fluid">

src/components/portfolioPage/GeographicMap.jsx
@@ -25,7 +25,7 @@ class GeographicMap extends React.PureComponent {
           }
           let portfolio = conf.portfolio;
           if (portfolio && portfolio[conf.user]) {
-            let layers = portfolio[conf.user].layers;
+            let layers = portfolio[conf.user].layers || [];
             this.setState({ layers });
           }
         }

src/components/portfolioPage/Portfolio.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import by from 'compare-func';
 import queryString from 'query-string';
 import Hypertopic from 'hypertopic';
@@ -9,6 +10,7 @@ import Header from '../Header.jsx';
 import Status from './Status.jsx';
 import SearchBar from './SearchBar.jsx';
 import ViewpointCreator from './ViewpointCreator.jsx';
+import VisitMap from './VisitMap.jsx';
 import { Trans } from '@lingui/macro';
 import { Items } from '../../model.js';
 import Selection from '../../Selection.js';
@@ -21,9 +23,16 @@ class Portfolio extends Component {
       corpora: [],
       items: [],
       selectedItems: [],
-      topicsItems: new Map()
+      topicsItems: new Map(),
+      visitMap: false
     };
     this._updateSelection();
+    conf.then(settings => {
+      if (settings.portfolio && settings.portfolio[settings.user])
+        this.setState({
+          visitMap: settings.portfolio[settings.user].visitMap || false,
+        });
+    });
   }
 
   render() {
@@ -34,6 +43,16 @@ class Portfolio extends Component {
       .map(([key, value]) => key.concat(' : ', value))
       .map(x => ({[x]: {name: x}}));
     let candidates = this.state.viewpoints.concat(attributes);
+    const urlParams = new URLSearchParams(window.location.search);
+    const selectionJSON = JSON.parse(urlParams.get('t'));
+    const visitPossible = this.state.visitMap
+      && selectionJSON
+      && selectionJSON.type === 'intersection'
+      && selectionJSON.data.length === 1
+      && selectionJSON.data[0].type === 'intersection'
+      && selectionJSON.data[0].selection.length === 1
+      && selectionJSON.data[0].exclusion.length === 0
+      && /^spatial : /.test(selectionJSON.data[0].selection[0]);
     return (
       <div className="App container-fluid">
         <Header conf={conf} />
@@ -56,7 +75,7 @@ class Portfolio extends Component {
                 </div>
               </div>
             </div>
-            {corpora}
+            {visitPossible ? <VisitMap items={this.state.selectedItems} /> : corpora}
           </div>
         </div>
       </div>
@@ -251,4 +270,4 @@ function push(map, topicId, itemId) {
     map.set(topicId, new Set([itemId]));
   }
 }
-export default Portfolio;
+export default withRouter(Portfolio);

src/components/portfolioPage/VisitMap.jsx
@@ -1,0 +1,118 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+class VisitMap extends Component {
+
+  _getItemCoordinates(input, nbFloors) {
+    const y = input % 100 === 0 ? Math.floor(input / 100) : nbFloors + Math.floor((input % 100 - 1) / 2);
+    const sign = 1 - 2 * (input % 2);
+    const x = input % 100 === 0 ? nbFloors : nbFloors + sign * (nbFloors - Math.floor(input / 100));
+    return {
+      x,
+      y
+    };
+  }
+
+  render() {
+    let { items } = this.props;
+    const inputs = items
+      .filter(item => /^[A-Z]+ ?\d*$/.test(item.name[0]))
+      .reduce((acc, item) => {
+        const results = item.name[0].match(/\d{3}$/);
+        if (results) {
+          acc.push({
+            item,
+            input: parseInt(results[0], 10)
+          });
+        }
+        return acc;
+      }, []);
+    const nbFloors = inputs.length ? Math.ceil(Math.max(...inputs.map(elt => elt.input)) / 100) : 0;
+    let selectedItems = inputs.map(elt => {
+      return {
+        ...(elt.item),
+        coordinates: this._getItemCoordinates(elt.input, nbFloors)
+      };
+    });
+    selectedItems = selectedItems
+      .map(x => x.name[0])
+      .map((x, i, arr) => arr.indexOf(x) === i && i)
+      .filter(x => selectedItems[x])
+      .map(x => selectedItems[x]);
+    const nbCols = 2 * nbFloors + 1;
+    const nbRows = inputs.length ? Math.max(...selectedItems.map(x => x.coordinates.y)) + 1 : 0;
+    const map = items.find(x => /^[A-Z]+$/.test(x.name[0]));
+    return (<div className="col-md-8 p-4">
+      <div className="Subject">
+        <h2 className="h4 font-weight-bold text-center">{selectedItems.length === 0 ? 'Chargement en cours' : 'Carte'}</h2>
+        <div className="m-4" style={{
+          display: 'grid',
+          gridTemplateColumns: `repeat(${nbFloors}, minmax(50px, 1fr)) minmax(100px, 2fr) repeat(${nbFloors}, minmax(50px, 1fr))`,
+          gridTemplateRows: `repeat(${nbRows + 1}, auto)`,
+          gridGap: '1rem',
+          overflowX: 'auto'
+        }}>{
+            new Array(Math.max(nbFloors - 1, 0)).fill(0).map((_, index) => <React.Fragment key={'column' + index}>
+              <div style={{
+                gridRow: '1 / 2',
+                gridColumn: `${index + 2} / ${index + 3}`
+              }}>{index === 0 ? <>1<sup>er</sup></> : <>{index + 1}<sup>ème</sup></>} étage</div>
+              <div style={{
+                gridRow: '1 / 2',
+                gridColumn: `${nbCols - index - 1} / ${nbCols - index}`
+              }}>{index === 0 ? <>1<sup>er</sup></> : <>{index + 1}<sup>ème</sup></>} étage</div>
+            </React.Fragment>)
+          }
+          <div style={{
+            gridRow: '1 / 2',
+            gridColumn: `${nbFloors + 1} / ${nbFloors + 2}`
+          }}>
+            Allée centrale
+          </div>
+          {
+            selectedItems.map(x => <Item key={x.id} data={x} />)
+          }
+          {
+            map && <Link id="plan" to={{
+              pathname: `/item/${map.corpus}/${map.id}`,
+              search: `?visit=${map.spatial[0]}`
+            }} style={{
+              gridColumn: `${nbFloors + 1} / ${nbFloors + 2}`,
+              gridRow: `${nbFloors + 2} / ${nbRows + 3}`,
+              margin: 'auto'
+            }}>
+              <img src={map.resource[0]} alt={map.name[0]}/>
+            </Link>
+          }
+        </div>
+      </div>
+    </div>);
+  }
+}
+
+class Item extends Component {
+
+  render() {
+    let {coordinates, corpus, id, spatial, thumbnail, name} = this.props.data;
+    return (
+      <div className="Item" style={{
+        gridColumn: `${coordinates.x + 1} / ${coordinates.x + 2}`,
+        gridRow: `${coordinates.y + 2} / ${coordinates.y + 3}`,
+        display: 'flex',
+        justifyContent: 'center'
+      }}>
+        <Link to={{
+          pathname: `/item/${corpus}/${id}`,
+          search: `?visit=${spatial[0]}`
+        }} >
+          <img src={thumbnail[0]} alt={name[0]} style={{
+            margin: 'auto'
+          }} />
+        </Link>
+      </div>
+    );
+  }
+
+}
+
+export default VisitMap;

commit faf91143f104284cce47e6d644e93ed22bdcd930

FIX: The "go back" link should not be labeled `{0}`.
The regression was caused by f68d4fedf3fa7b0ccafcb0f1a8f77db7fbb2962e.
It get unnoticed before being on production because it didn't affect the development version (launched with `npm start`).

src/components/itemPage/Item.jsx
@@ -72,7 +72,7 @@ class Item extends Component {
             search: decodeURIComponent(goBackUrlParams.toString())
           }} className="badge badge-pill badge-light TopicTag">
             <span className="badge badge-pill badge-dark oi oi-chevron-left"> </span>
-            <Trans>{visitName ? 'Retour à la visite' : 'Retour à l\'accueil'}</Trans>
+            { visitName ? t`Retour à la visite` : t`Retour à l'accueil` }
           </Link>
         </div>
         <div className="container-fluid">